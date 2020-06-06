c69f446b2e7d7c5f9188bc3234826550be79a2bf
ElPlaso <66361666+ElPlaso@users.noreply.github.com>
2020-06-07 11:32:25 +1200

    Update robot.cpp

--- robot.cpp
+++ robot.cpp
@@ -3,14 +3,24 @@ int main(){
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-    double vLeft = 40.0;
-    double vRight = 30.0;
+    double vLeft = 3.0;
+    double vRight = 3.0;
     takePicture();
     SavePPMFile("i0.ppm",cameraView);
     while(1){
+		takePicture();
+		double error = 0.0;
+		for (int i =0; i < 150; i++){
+			int pix = get_pixel(cameraView,50,i,3);
+			int isWhite;
+			if( pix > 250){isWhite = 1;
+				error = error;}
+			else {isWhite = 0;}	
+		}
+		
+		vLeft;
+		vRight;
       setMotors(vLeft,vRight);   
       std::cout<<" vLeft="<<vLeft<<"  vRight="<<vRight<<std::endl;
        usleep(10000);
   } //while
-
-} // main
